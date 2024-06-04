444b82a6f5851816d5a9288e3d38e88e718651af
unknown <rafv540@gmail.com>
2024-06-03 21:00:19 -0400

    more spacing added

--- src/app/components/ui/MostPopularVideos.tsx
+++ src/app/components/ui/MostPopularVideos.tsx
@@ -1,9 +1,9 @@
-import VideoCard from './VideoCard';
+import VideoCard from "./VideoCard";
 
-import { VideoListResponse } from '@/app/types';
+import { VideoListResponse } from "@/app/types";
 
 let baseUrl =
-	'https://www.googleapis.com/youtube/v3/videos?&key=AIzaSyBeimpcogwB9SHkIhrfSdxkzLc27EKLFgc&chart=mostPopular&part=snippet';
+	"https://www.googleapis.com/youtube/v3/videos?&key=AIzaSyBeimpcogwB9SHkIhrfSdxkzLc27EKLFgc&chart=mostPopular&part=snippet";
 
 async function getData() {
 	const res = await fetch(baseUrl);
@@ -12,7 +12,7 @@ async function getData() {
 
 	if (!res.ok) {
 		// This will activate the closest `error.js` Error Boundary
-		throw new Error('Failed to fetch data');
+		throw new Error("Failed to fetch data");
 	}
 
 	return res.json();
@@ -23,15 +23,15 @@ export default async function MostPopularVideos() {
 	const popularVideos = response.items;
 
 	return (
-		<section className='w-full flex flex-col'>
-			<div className='mb-4 px-4 md:px-0'>
-				<h3 className='text-lg text-zinc-200 font-semibold'>Trending videos</h3>
-				<p className='text-zinc-400 text-sm'>
+		<section className="w-full flex flex-col">
+			<div className="mb-4 px-4 md:px-0">
+				<h3 className="text-lg text-zinc-200 font-semibold">Trending videos</h3>
+				<p className="text-zinc-400 text-sm">
 					Most popular videos at the moment
 				</p>
 			</div>
 
-			<div className='mx-auto w-full grid grid-cols-1 gap-y-4 md:grid-cols-4 md:gap-x-4 lg:gap-y-10 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10'>
+			<div className="mx-auto w-full grid grid-cols-1 gap-y-8 md:grid-cols-4 md:gap-x-4 lg:gap-y-10 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10">
 				{popularVideos.map((item) => (
 					<VideoCard
 						key={item.id}
--- src/app/video/[videoId]/page.tsx
+++ src/app/video/[videoId]/page.tsx
@@ -31,7 +31,7 @@ export default async function Page({
 	if (!video) return <>Not available</>;
 
 	return (
-		<main className="pt-16 w-full min-h-screen flex flex-col md:px-8 lg:pl-14 lg:px-6 lg:flex-row lg:gap-x-6 xl:px-16 2xl:px-24">
+		<main className="pt-16 pb-16 w-full min-h-screen flex flex-col md:px-8 lg:pl-14 lg:px-6 lg:flex-row lg:gap-x-6 xl:px-16 2xl:px-24">
 			{/* first column */}
 			<div className="min-h-screen lg:w-4/5">
 				{/* video */}
